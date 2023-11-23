68d95c66c8bb04ae8a0c968aef4feca9ecd1117d
Tridiv <tridiv_b@yahoo.com>
2023-11-23 11:19:52 +0100

    Format codestyle, add clang-format, update gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,95 @@
-#include <optional>
+#include <io2d.h>
+
 #include <fstream>
 #include <iostream>
-#include <vector>
+#include <optional>
 #include <string>
-#include <io2d.h>
-#include "route_model.h"
+#include <vector>
+
 #include "render.h"
+#include "route_model.h"
 #include "route_planner.h"
 
 using namespace std::experimental;
 
-static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
-{   
-    std::ifstream is{path, std::ios::binary | std::ios::ate};
-    if( !is )
-        return std::nullopt;
-    
-    auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
-    
-    is.seekg(0);
-    is.read((char*)contents.data(), size);
+static std::optional<std::vector<std::byte>> ReadFile(const std::string& path) {
+  std::ifstream is{path, std::ios::binary | std::ios::ate};
+  if (!is) return std::nullopt;
 
-    if( contents.empty() )
-        return std::nullopt;
-    return std::move(contents);
+  auto size = is.tellg();
+  std::vector<std::byte> contents(size);
+
+  is.seekg(0);
+  is.read((char*)contents.data(), size);
+
+  if (contents.empty()) return std::nullopt;
+  return std::move(contents);
 }
 
-float set_input(std::string message){
-    float value;
-    std::cout << message << std::endl;
+float set_input(std::string message) {
+  float value;
+  std::cout << message << std::endl;
+  std::cin >> value;
+  while (value < 0 || value > 100) {
+    std::cout << "Invalid input. Please input one between 0 and 100"
+              << std::endl;
     std::cin >> value;
-    while (value < 0 || value > 100){
-        std::cout << "Invalid input. Please input one between 0 and 100" << std::endl;
-        std::cin >> value;
-    }
-    return value;
+  }
+  return value;
 }
 
-int main(int argc, const char **argv)
-{    
-    std::string osm_data_file = "";
-    if( argc > 1 ) {
-        for( int i = 1; i < argc; ++i )
-            if( std::string_view{argv[i]} == "-f" && ++i < argc )
-                osm_data_file = argv[i];
-    }
-    else {
-        std::cout << "To specify a map file use the following format: " << std::endl;
-        std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
-        osm_data_file = "../map.osm";
-    }
-    
-    std::vector<std::byte> osm_data;
- 
-    if( osm_data.empty() && !osm_data_file.empty() ) {
-        std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
-        auto data = ReadFile(osm_data_file);
-        if( !data )
-            std::cout << "Failed to read." << std::endl;
-        else
-            osm_data = std::move(*data);
-    }
+int main(int argc, const char** argv) {
+  std::string osm_data_file = "";
+  if (argc > 1) {
+    for (int i = 1; i < argc; ++i)
+      if (std::string_view{argv[i]} == "-f" && ++i < argc)
+        osm_data_file = argv[i];
+  } else {
+    std::cout << "To specify a map file use the following format: "
+              << std::endl;
+    std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
+    osm_data_file = "../map.osm";
+  }
 
-    float start_x = set_input("Enter starting point x-coordinate: ");
-    float start_y = set_input("Enter starting point y-coordinate: ");
-    float end_x = set_input("Enter end point x-coordinate: ");
-    float end_y = set_input("Enter end point x-coordinate: ");
+  std::vector<std::byte> osm_data;
 
-    // Build Model.
-    RouteModel model{osm_data};
+  if (osm_data.empty() && !osm_data_file.empty()) {
+    std::cout << "Reading OpenStreetMap data from the following file: "
+              << osm_data_file << std::endl;
+    auto data = ReadFile(osm_data_file);
+    if (!data)
+      std::cout << "Failed to read." << std::endl;
+    else
+      osm_data = std::move(*data);
+  }
 
-    // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
-    route_planner.AStarSearch();
+  float start_x = set_input("Enter starting point x-coordinate: ");
+  float start_y = set_input("Enter starting point y-coordinate: ");
+  float end_x = set_input("Enter end point x-coordinate: ");
+  float end_y = set_input("Enter end point x-coordinate: ");
 
-    std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+  // Build Model.
+  RouteModel model{osm_data};
 
-    // Render results of search.
-    Render render{model};
+  // Create RoutePlanner object and perform A* search.
+  RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
+  route_planner.AStarSearch();
 
-    auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
-    display.size_change_callback([](io2d::output_surface& surface){
-        surface.dimensions(surface.display_dimensions());
-    });
-    display.draw_callback([&](io2d::output_surface& surface){
-        render.Display(surface);
-    });
-    display.begin_show();
+  std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
+
+  // Render results of search.
+  Render render{model};
+
+  auto display = io2d::output_surface{400,
+                                      400,
+                                      io2d::format::argb32,
+                                      io2d::scaling::none,
+                                      io2d::refresh_style::fixed,
+                                      30};
+  display.size_change_callback([](io2d::output_surface& surface) {
+    surface.dimensions(surface.display_dimensions());
+  });
+  display.draw_callback(
+      [&](io2d::output_surface& surface) { render.Display(surface); });
+  display.begin_show();
 }
--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,84 +1,90 @@
-#include "route_planner.h"
 #include <algorithm>
 
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
-    // Convert inputs to percentage:
-    start_x *= 0.01;
-    start_y *= 0.01;
-    end_x *= 0.01;
-    end_y *= 0.01;
+#include "route_planner.h"
 
-    start_node = &(m_Model.FindClosestNode(start_x, start_y));
-    end_node = &(m_Model.FindClosestNode(end_x, end_y));
+RoutePlanner::RoutePlanner(
+    RouteModel &model, float start_x, float start_y, float end_x, float end_y)
+    : m_Model(model) {
+  // Convert inputs to percentage:
+  start_x *= 0.01;
+  start_y *= 0.01;
+  end_x *= 0.01;
+  end_y *= 0.01;
 
+  start_node = &(m_Model.FindClosestNode(start_x, start_y));
+  end_node = &(m_Model.FindClosestNode(end_x, end_y));
 }
 
-
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
-    return node->distance(*end_node);
+  return node->distance(*end_node);
 }
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
-    current_node->FindNeighbors();
-    for (auto& neighbour : current_node->neighbors){
-        neighbour->parent = current_node;
-        neighbour->g_value = current_node->g_value + neighbour->distance(*current_node);
-        neighbour->h_value = CalculateHValue(neighbour);
-        open_list.push_back(neighbour);
-        neighbour->visited = true;
-    }
-
+  current_node->FindNeighbors();
+  for (auto &neighbour : current_node->neighbors) {
+    neighbour->parent = current_node;
+    neighbour->g_value =
+        current_node->g_value + neighbour->distance(*current_node);
+    neighbour->h_value = CalculateHValue(neighbour);
+    open_list.push_back(neighbour);
+    neighbour->visited = true;
+  }
 }
 
 RouteModel::Node *RoutePlanner::NextNode() {
-    // sort the elements in descending order
-    std::sort(open_list.begin(), open_list.end(), [](const RouteModel::Node* node1, const RouteModel::Node* node2){return node1->g_value + node1->h_value > node2->g_value + node2->h_value;});
+  // sort the elements in descending order
+  std::sort(open_list.begin(),
+            open_list.end(),
+            [](const RouteModel::Node *node1, const RouteModel::Node *node2) {
+              return node1->g_value + node1->h_value >
+                     node2->g_value + node2->h_value;
+            });
 
-    auto current_node = open_list.back();
-    open_list.pop_back();
+  auto current_node = open_list.back();
+  open_list.pop_back();
 
-    return current_node;
-
+  return current_node;
 }
 
-std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
-    // Create path_found vector
-    distance = 0.0f;
-    std::vector<RouteModel::Node> path_found;
+std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(
+    RouteModel::Node *current_node) {
+  // Create path_found vector
+  distance = 0.0f;
+  std::vector<RouteModel::Node> path_found;
 
-    while (current_node != nullptr) {
-        path_found.push_back(*current_node);
-        if (current_node->parent != nullptr){
-            distance += current_node->distance(*current_node->parent);
-        }
-        current_node = current_node->parent;
+  while (current_node != nullptr) {
+    path_found.push_back(*current_node);
+    if (current_node->parent != nullptr) {
+      distance += current_node->distance(*current_node->parent);
     }
-    // path_found.push_back(*current_node->parent);
-    std::reverse(path_found.begin(), path_found.end());
+    current_node = current_node->parent;
+  }
+  // path_found.push_back(*current_node->parent);
+  std::reverse(path_found.begin(), path_found.end());
 
-    distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
-    return path_found;
-
+  distance *= m_Model.MetricScale();  // Multiply the distance by the scale of
+                                      // the map to get meters.
+  return path_found;
 }
 
 void RoutePlanner::AStarSearch() {
-    RouteModel::Node *current_node = nullptr;
+  RouteModel::Node *current_node = nullptr;
 
-    if (start_node->distance(*end_node) == 0){
-        std::cout << "Start and end coordinates are same. No path search required." << std::endl;
-        
+  if (start_node->distance(*end_node) == 0) {
+    std::cout << "Start and end coordinates are same. No path search required."
+              << std::endl;
+
+  } else {
+    AddNeighbors(start_node);
+    start_node->visited = true;
+    while (!open_list.empty()) {
+      current_node = NextNode();
+      if (current_node->distance(*end_node) == 0) {
+        m_Model.path = ConstructFinalPath(current_node);
+        // break if the goal is reached before the open list is empty
+        break;
+      }
+      AddNeighbors(current_node);
     }
-    else {
-        AddNeighbors(start_node);
-        start_node->visited = true;
-        while (!open_list.empty()) {
-            current_node = NextNode();
-            if (current_node->distance(*end_node) == 0){
-                m_Model.path = ConstructFinalPath(current_node);
-                // break if the goal is reached before the open list is empty
-                break;
-            }
-            AddNeighbors(current_node);
-        }
-    }
+  }
 }
